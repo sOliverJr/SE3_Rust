f2ee65ff51289b16f96adc8e8e38e4f743a82980
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2022-12-22 12:12:38 +0100

    final commit

--- macro.rs
+++ macro.rs
@@ -1,1 +1,19 @@
+fn main() { 
+  let _v1: Vec<u32> = vec![1, 2, 3];
+  let _v2: Vec<u32> = vec![1, 2, 3, 4, 5, 6];
+  let _v3: Vec<&str> = my_vec!["A", "B", "C", "D", "E"];
+}
 
+#[macro_export]
+macro_rules! my_vec {
+  ( $( $x:expr ),* ) => {
+    {
+      let mut temp_vec = Vec:: new();
+      $(
+        temp_vec.push ($x);
+        println!($x);
+      )*
+      temp_vec
+    }
+  };
+}
--- structures.rs
+++ structures.rs
@@ -1,2 +1,12 @@
 struct Rechteck {
+  breite: u32,
+  höhe: u32,
+}
+
+
+fn main(){
+  let _rechteck = Rechteck {
+    breite: 30,
+    höhe: 40
+  };
 }
